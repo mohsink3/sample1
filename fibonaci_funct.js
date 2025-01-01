c40eda59238f9d5af5a2410f60113e3931f864c2
mohsin khan <mohsin21.kh@gmail.com>
2025-01-01 09:38:23 +0530

    program to print fib funtion

---
+++ fibonaci_funct.js
@@ -0,0 +1,17 @@
+function printfib(n) {
+    let first = 1;
+    let second = 1;
+    console.log(first);
+    console.log(second);
+    let sum;
+    for (let i = 1; i <= n-2; i++) {
+        sum = first + second;
+        console.log(sum);
+        first = second;
+        second = sum
+    }
+}
+
+printfib(5);
+
+
