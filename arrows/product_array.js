68e56c00c4c35dd3a0ae782e1eb68427d57d4143
mohsin khan <mohsin21.kh@gmail.com>
2025-01-20 16:11:15 +0530

    product_array commited

---
+++ arrows/product_array.js
@@ -0,0 +1,14 @@
+const products = [{
+    name:"laptop",price:50000,stock:15},
+    {name:"mobile",price:15000,stock:8},
+    {name:"headphone",price:2000,stock:20},
+    {names:"keyboard",price:1000,stock:5}];
+
+const product1 = products.filter(product => product.stock>10);
+const product2 = products.map(product =>product.price *1.5);
+console.log(products);
+console.log(product1);
+console.log(product2);
+
+
+
